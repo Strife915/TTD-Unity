001ac24b6f4403963353335b68e544e5a364545b
Strife915 <thekarabulut95@gmail.com>
2022-07-25 01:58:17 +0300

    feat:Playercontroller implemented

---
+++ TTD-Unity-2D/Assets/GameFolder/Scripts/Concrete/PlayerController.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Unity_TDD_Controllers
+{
+    public class PlayerController : MonoBehaviour
+    {
+    }
+}
