2ab9d47328d0ac4852dfde3a450cdbce609c9b35
Strife915 <thekarabulut95@gmail.com>
2022-08-06 21:39:24 +0300

    feat:player one time jump on several input test implemented with BlockDisableHelper.cs

--- TTD-Unity-2D/Assets/GameFolder/Scripts/Tests/PlayMode/player_jump.cs
+++ TTD-Unity-2D/Assets/GameFolder/Scripts/Tests/PlayMode/player_jump.cs
@@ -8,65 +8,69 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 
-public class player_jump : MonoBehaviour
+namespace TDDBeginner.Movement
 {
-    [UnityTest]
-    public IEnumerator player_jump_one_time()
+    public class player_jump : MonoBehaviour
     {
-        yield return SceneManager.LoadSceneAsync("PlayerMovementTest");
-        var player = GameObject.FindObjectOfType<PlayerController>();
-        player.InputReader = Substitute.For<IInputReader>();
+        [UnityTest]
+        public IEnumerator player_jump_one_time()
+        {
+            yield return SceneManager.LoadSceneAsync("PlayerMovementTest");
+            var player = GameObject.FindObjectOfType<PlayerController>();
+            player.InputReader = Substitute.For<IInputReader>();
 
-        yield return null;
-        float yStartPosition = player.transform.position.y;
+            yield return null;
+            float yStartPosition = player.transform.position.y;
 
-        player.InputReader.Jump.Returns(true);
-        yield return null;
-        player.InputReader.Jump.Returns(false);
+            player.InputReader.Jump.Returns(true);
+            yield return null;
+            player.InputReader.Jump.Returns(false);
 
-        yield return new WaitForSeconds(2f);
-        Assert.Greater(player.transform.position.y, yStartPosition);
-    }
+            yield return new WaitForSeconds(1f);
+            Assert.Greater(player.transform.position.y, yStartPosition);
+        }
 
-    [UnityTest]
-    public IEnumerator player_jump_one_time_on_multiple_input()
-    {
-        yield return SceneManager.LoadSceneAsync("PlayerMovementTest");
-        var player = GameObject.FindObjectOfType<PlayerController>();
-        player.InputReader = Substitute.For<IInputReader>();
+        [UnityTest]
+        public IEnumerator player_jump_one_time_on_multiple_input()
+        {
+            yield return SceneManager.LoadSceneAsync("PlayerMovementTest");
+            var player = GameObject.FindObjectOfType<PlayerController>();
+            player.InputReader = Substitute.For<IInputReader>();
+            var blockDisableHelper = GameObject.FindObjectOfType<BlockDisableHelper>();
 
-        yield return null;
-        float yStartPosition = player.transform.position.y;
+            yield return null;
+            float yStartPosition = player.transform.position.y;
 
-        player.InputReader.Jump.Returns(true);
-        yield return null;
+            player.InputReader.Jump.Returns(true);
+            yield return null;
+            blockDisableHelper.SetDisableCollider();
+            yield return new WaitForSeconds(2f);
+            Assert.LessOrEqual(player.transform.position.y, yStartPosition);
+        }
 
-        yield return new WaitForSeconds(2f);
-        Assert.LessOrEqual(player.transform.position.y, yStartPosition);
-    }
+        [UnityTest]
+        public IEnumerator player_jump_one_time_wait_after_jump_again()
+        {
+            yield return SceneManager.LoadSceneAsync("PlayerMovementTest");
+            var player = GameObject.FindObjectOfType<PlayerController>();
+            player.InputReader = Substitute.For<IInputReader>();
 
-    [UnityTest]
-    public IEnumerator player_jump_one_time_wait_after_jump_again()
-    {
-        yield return SceneManager.LoadSceneAsync("PlayerMovementTest");
-        var player = GameObject.FindObjectOfType<PlayerController>();
-        player.InputReader = Substitute.For<IInputReader>();
+            yield return null;
+            float yStartPosition = player.transform.position.y;
 
-        yield return null;
-        float yStartPosition = player.transform.position.y;
-
-        player.InputReader.Jump.Returns(true);
-        yield return null;
-        player.InputReader.Jump.Returns(false);
+            player.InputReader.Jump.Returns(true);
+            yield return null;
+            player.InputReader.Jump.Returns(false);
 
 
-        yield return new WaitForSeconds(2f);
+            yield return new WaitForSeconds(2f);
 
-        player.InputReader.Jump.Returns(true);
-        yield return null;
-        player.InputReader.Jump.Returns(false);
-        yield return new WaitForSeconds(1f);
+            player.InputReader.Jump.Returns(true);
+            yield return null;
+            player.InputReader.Jump.Returns(false);
+            yield return new WaitForSeconds(1f);
 
-        Assert.Greater(player.transform.position.y, yStartPosition);
+            Assert.Greater(player.transform.position.y, yStartPosition);
+        }
     }
 }
